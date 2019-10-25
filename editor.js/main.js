bea4ad473ba200239e0ac7547001ddaefff60881
Md Shayon <mdshayon0@gmail.com>
2019-10-25 15:18:20 +0600

    editor js save data to the database

--- editor.js/main.js
+++ editor.js/main.js
@@ -1,19 +1,47 @@
-import EditorJS from '@editorjs/editorjs'; 
-import Header from '@editorjs/header'; 
-import List from '@editorjs/list'; 
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import Embed from '@editorjs/embed';
 
-const editor = new EditorJS({ 
+const editor = new EditorJS({
   /** 
    * Id of Element that should contain the Editor 
-   */ 
-  holderId: 'codex-editor', 
-  
+   */
+  holderId: 'codex-editor',
+
   /** 
    * Available Tools list. 
    * Pass Tool's class or Settings object for each Tool you want to use 
-   */ 
-  tools: { 
-    header: Header, 
-    list: List 
-  }, 
+   */
+  tools: {
+    header: {
+      class: Header,
+      inlineToolbar: ['link']
+    },
+    list: {
+      class: List,
+      inlineToolbar: ['link', 'blod']
+    }
+  },
+  embed: {
+    class: Embed,
+    inlineToolbar: false,
+    config: {
+      services: {
+        youtube: true,
+        coub: true
+      }
+    }
+  }
+
+
+});
+
+
+let saveBtn = document.querySelector('button');
+saveBtn.addEventListener('click', ()=>{
+  editor.save().then(outputData=>{
+    console.log("Article data: ", outputData);
+    
+  })
 })
