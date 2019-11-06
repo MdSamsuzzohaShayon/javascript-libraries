4537664512bc3b8dbf535c844c32541ce59d07d5
Md Shayon <mdshayon0@gmail.com>
2019-11-06 16:32:49 +0600

    lightgallery js

---
+++ Lightgallery.js/js/main.js
@@ -0,0 +1,14 @@
+lightGallery(document.getElementById('lightgallery'), {
+    mode: 'lg-fade',
+    cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'    ,
+    // loop: true,
+    controls: true,
+    fullScreen: true,
+    autoplay: true
+});
+
+// const light_gallery = document.getElementById('lightgallery');
+// light_gallery.lightGallery({
+//     mode: 'lg-fade',
+//     cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'
+// });
